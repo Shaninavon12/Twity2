7005e8b83fee74d7b21a2721c7fbf829846588f8
odev954 <kleinbottle175@gmail.com>
2023-05-28 14:47:52 +0300

    adding cr comments

--- loginPage.js
+++ loginPage.js
@@ -1,15 +1,25 @@
 function LoginUser() {
-    const registeredUsers = JSON.parse(localStorage.getItem('Users'));
-    const givenUser = document.getElementById("username").value;
-    const givenPassword = document.getElementById("password").value;
-    const mappedUserProperty =Object.fromEntries(
-        registeredUsers.map(e => [e.name , e.password])
-    );
-    for (const key in mappedUserProperty){
-        if(key == givenUser && mappedUserProperty[key] == givenPassword){
-            alert('login successful');
-            window.location("./poster.html");
-        }
-    }
-    alert('login fail');
+	const registeredUsers = JSON.parse(localStorage.getItem("Users"));
+	const givenUser = document.getElementById("username").value;
+	const givenPassword = document.getElementById("password").value;
+	// CR minor [clarity]: what is e?
+	// rename this variable to a more indicative name
+	// CR minor [clean code]: why you are not saving the user details in a format that
+	// wouldn't require you to do extra operations? just build an object that is
+	// better suites you that you can use directly with processing
+	const mappedUserProperty = Object.fromEntries(
+		registeredUsers.map((e) => [e.name, e.password])
+	);
+	// CR minor [syntax]: you can just retrieve the keys with Object.keys()
+	for (const key in mappedUserProperty) {
+		// CR minor [syntax]: never use "==" because it will converts the operands to strings first and
+		// then will compare between them. for a better comparison use "===" operator instead.
+		if (key == givenUser && mappedUserProperty[key] == givenPassword) {
+			alert("login successful");
+			// CR MAJOR [syntax]: TypeError - window.location is not a function
+			// you should use this property as you used it in homePage.js
+			window.location("./poster.html");
+		}
+	}
+	alert("login fail");
 }
--- poster.js
+++ poster.js
@@ -1,12 +1,11 @@
-function addNewTwits(){
-    var newTwits = document.getElementById("newTwit").value;
-    if (newTwits != "" ){
-        var newTwit = document.createElement('li');
-        newTwit.appendChild (document.createTextNode(newTwits));
-        document.querySelector('ul').appendChild(newTwit);
-    }
-    else
-    {
-        alert("you have to write something");
-    }   
+function addNewTwits() {
+	// CR MAJOR [logic]: there is no need to save the tweets in the local storage?
+	var newTwits = document.getElementById("newTwit").value;
+	if (newTwits != "") {
+		var newTwit = document.createElement("li");
+		newTwit.appendChild(document.createTextNode(newTwits));
+		document.querySelector("ul").appendChild(newTwit);
+	} else {
+		alert("you have to write something");
+	}
 }
